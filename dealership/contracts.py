e9659e928b98e67b7ec8dc350033c9efa5d734f5
Nikhila Yadunath <nikihapy@gmail.com>
2016-11-12 18:58:04 +0000

    Main and test functionality for Dealership class complete

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,75 @@
+from vehicles import Car, Truck, Motorcycle
+from customers import*
+
+
 class Contract(object):
     pass
+    # @classmethod
+    # def total_value(cls):
+    #     cls.vehicle.sale_price() 
+    #     #BUY CONTRACT: vehicle.sale_price() + (I * monthly_payments * sale_price() / 100) - (discount if employee)
+        #LEASE CONTRACT: vehicle.sale_price() + (lease_multiplier) - (discount if employee)
+
+    #@classmethod
+    # def monthly_value(cls):
+    #     return cls.total_value() / cls.monthly_payments
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
-
+        self.vehicle=vehicle
+        self.customer=customer
+        self.monthly_payments=monthly_payments
+        
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
+        
+    
+    def total_value(self):
+        multiplier = 0       
+        discount_percentage = 1
+        
+        if self.customer.is_employee():
+            discount_percentage = .9       
+       
+        if isinstance(self.vehicle, Car):
+            multiplier = 1.07
+        elif isinstance(self.vehicle, Motorcycle):
+            multiplier = 1.03
+        else: 
+            multiplier = 1.11
+            
+        price = self.vehicle.sale_price() + (multiplier * self.monthly_payments * self.vehicle.sale_price() / 100) 
+        
+        return price * discount_percentage
+     
+     
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle=vehicle
+        self.customer=customer
+        self.length_in_months=length_in_months
+        
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months   
+        
+    def total_value(self):
+        multiplier = 0
+        discount_percentage = 1
+        
+        if self.customer.is_employee():
+            discount_percentage = .9
+            
+        if isinstance(self.vehicle, Car):
+            multiplier = 1.2
+        elif isinstance(self.vehicle, Motorcycle):
+            multiplier = 1
+        else: 
+            multiplier = 1.7
+        
+        
+        price = self.vehicle.sale_price() + (self.vehicle.sale_price() * multiplier / self.length_in_months)
+        
+        return price * discount_percentage
+            
+   
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,27 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
-
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+    
+    def sale_price(self):
+        return self.base_price * self.sale_multipliers
+    
+    def purchase_price(self):
+        return self.sale_price() - (self.purchase_multipliers * self.miles)
+        
+        
 class Car(Vehicle):
-    pass
-
+    sale_multipliers = 1.2
+    purchase_multipliers = 0.004
 
 class Motorcycle(Vehicle):
-    pass
-
+    sale_multipliers = 1.1
+    purchase_multipliers = 0.009
 
 class Truck(Vehicle):
-    pass
+    sale_multipliers = 1.6
+    purchase_multipliers = 0.02
+
