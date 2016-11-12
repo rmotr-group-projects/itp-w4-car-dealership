c45ee191fe1c9561fb88b12a4bb030470fd513c2
amerca <amerca3@gmail.com>
2016-11-12 16:46:50 +0000

    car dealership

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,73 @@
+from vehicles import *
+from customers import *
+
 class Contract(object):
     pass
 
 
 class BuyContract(Contract):
+    #vehicle.sale_price( vehicle.purchase_price(
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle= vehicle
+        self.customer= customer
+        self.monthly_payments= monthly_payments
+#vehicle.sale_price() + (I * monthly_payments * sale_price() / 100) - (discount if employee)
+    def total_value(self):
+        if isinstance(self.vehicle,Car):
+            I=1.07
+        elif isinstance(self.vehicle, Truck):
+            I=1.11
+        elif isinstance(self.vehicle, Motorcycle): 
+            I=1.03
+        if self.customer.is_employee():
+            discount=(self.vehicle.sale_price()+(I*self.monthly_payments*self.vehicle.sale_price()/100))*.1
+        else:
+            discount=0
+        return self.vehicle.sale_price()+(I*self.monthly_payments*self.vehicle.sale_price()/100)-discount
+    
+    def monthly_value(self):
+        return self.total_value()/self.monthly_payments
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle= vehicle
+        self.customer= customer
+        self.length_in_months= length_in_months
+    
+    '''
+    Lease Multipliers:
+    
+    Car: sale_price() + (sale_price() * 1.2 / length_in_months).
+    Motorcycle: sale_price() + (sale_price() * 1 / length_in_months)
+    Truck: sale_price() + (sale_price() * 1.7 / length_in_months)
+    '''
+
+    def total_value(self):
+        '''vehicle.sale_price() + (lease_multiplier) - (discount if employee)'''
+        if isinstance(self.vehicle, Car):
+            lease_multiplier = self.vehicle.sale_price() + (self.vehicle.sale_price() * 1.2 / self.length_in_months)
+
+        elif isinstance(self.vehicle, Motorcycle):
+            lease_multiplier = self.vehicle.sale_price() + (self.vehicle.sale_price() * 1 / self.length_in_months)
+
+        elif isinstance(self.vehicle, Truck):
+            lease_multiplier = self.vehicle.sale_price() + (self.vehicle.sale_price() * 1.7 / self.length_in_months)
+        
+        else:
+            lease_multiplier = 0
+        
+        
+        #check if the customer is an employee. If so, apply a 10% discount on the final price
+        if self.customer.is_employee():
+            discount = (self.vehicle.sale_price() + lease_multiplier) * 0.1
+        
+        else:
+            discount = 0
+
+        return lease_multiplier - discount
+    
+    
+    def monthly_value(self):
+        '''total value / length_in_months'''
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,45 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
+        self.maker=maker
+        self.model=model
+        self.year=year
+        self.base_price=base_price
+        self.miles=miles
+        self.S = 1
+        self.P = 1
+    
+    def sale_price(self):
+        return self.S * self.base_price
+    
+    def purchase_price(self):
+        return (self.sale_price() - (self.P * self.miles)) 
 
 class Car(Vehicle):
-    pass
-
+    def __init__(self, maker, model, year, base_price, miles):
+        #inherits the __init__ method to initialize attributes
+        super(Car, self).__init__(maker, model, year, base_price, miles)
+        self.S = 1.2
+        self.P = 0.004
+    
+    def __repr__(self):
+        return '<car:{}>'.format(self.maker)
 
 class Motorcycle(Vehicle):
-    pass
-
+    def __init__(self, maker, model, year, base_price, miles):
+        #inherits the __init__ method to initialize attributes
+        super(Motorcycle, self).__init__(maker, model, year, base_price, miles)
+        self.S = 1.1
+        self.P = 0.009
+    
+    def __repr__(self):
+        return '<moto:{}>'.format(self.maker)
 
 class Truck(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        #inherits the __init__ method to initialize attributes
+        super(Truck, self).__init__(maker, model, year, base_price, miles)
+        self.S = 1.6
+        self.P = 0.02
+    
+    def __repr__(self):
+        return '<truck:{}>'.format(self.maker)
