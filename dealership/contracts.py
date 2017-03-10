277a982be7d73be70b71a2dff1de03f5bf0f1a76
Francine Biazin do Nascimento <francine.biazin@gmail.com>
2017-03-10 17:03:30 +0000

    Initial commit

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,58 @@
+from .vehicles import Car, Truck, Motorcycle
+from .customers import Customer, Employee
+
 class Contract(object):
-    pass
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract, self).__init__(vehicle, customer)
+        self.monthly_payments = monthly_payments
+
+
+    def total_value(self):
+        sale_price = self.vehicle.sale_price()
+        
+        if self.vehicle.__class__ == Car:
+            price = sale_price + (1.07 * self.monthly_payments * sale_price / 100)
+        elif self.vehicle.__class__ == Motorcycle:
+            price = sale_price + (1.03 * self.monthly_payments * sale_price / 100)
+        elif self.vehicle.__class__ == Truck:
+            price = sale_price + (1.11 * self.monthly_payments * sale_price / 100)
+        
+        if self.customer.is_employee():
+            return price - (price * 0.1)
+        else:
+            return price
+
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+
+
+    def total_value(self):
+        sale_price = self.vehicle.sale_price()
+
+        if self.vehicle.__class__ == Car:
+            price = sale_price + (sale_price * 1.2 / self.length_in_months)
+        elif self.vehicle.__class__ == Motorcycle:
+            price = sale_price + (sale_price * 1 / self.length_in_months)
+        elif self.vehicle.__class__ == Truck:
+            price = sale_price + (sale_price * 1.7 / self.length_in_months)
+        
+        if self.customer.is_employee():
+            return price - (price * 0.1)
+        else:
+            return price
+
+
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,31 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
+    def sale_price(self):
+        return self.base_price * 1.2
+    
+    def purchase_price(self):
+        return Car.sale_price(self) - (0.004 * self.miles)
 
 
 class Motorcycle(Vehicle):
-    pass
+    def sale_price(self):
+        return self.base_price * 1.1
+    
+    def purchase_price(self):
+        return Motorcycle.sale_price(self) - (0.009 * self.miles)
 
 
 class Truck(Vehicle):
-    pass
+    def sale_price(self):
+        return self.base_price * 1.6
+    
+    def purchase_price(self):
+        return Truck.sale_price(self) - (0.02 * self.miles)
