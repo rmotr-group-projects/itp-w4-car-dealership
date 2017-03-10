913711c21687455ccc6e2cff6ad2696074b4e1b8
Brian Park <jwpark05@gmail.com>
2017-03-10 03:38:55 +0000

    initial_commit

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,46 @@
 class Contract(object):
     pass
-
-
+   
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
-
+        self.customer = customer
+        self.vehicle = vehicle
+        self.monthly_payments = monthly_payments
+        
+        
+    def total_value(self):
+        total_val = self.vehicle.sale_price() + (self.vehicle.interest * self.monthly_payments * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee() is True: 
+            return total_val * 0.9
+        else:
+            return total_val
+            
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
+        
 class LeaseContract(Contract):
+    
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.customer = customer
+        self.vehicle = vehicle
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+        total_val = self.vehicle.sale_price() + (self.vehicle.sale_price() * self.vehicle.lease_multiplier / self.length_in_months) 
+        if self.customer.is_employee() is True:
+            return total_val * 0.9
+        else:
+            return total_val
+    
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
+            
+
+
+"""The total_value() of a LeaseContract will be computed in this way: vehicle.sale_price() + (lease_multiplier) - (discount if employee) *.
+In this case lease_multiplier depends on the vehicle and is computed in the following way:
+
+Car: sale_price() + (sale_price() * 1.2 / length_in_months).
+Motorcycle: sale_price() + (sale_price() * 1 / length_in_months)
+Truck: sale_price() + (sale_price() * 1.7 / length_in_months)
+The monthly_value of the contract will be just the total value divided by the amount of months: total_value() / length_in_months."""
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,44 @@
 class Vehicle(object):
+    
+    sale_multiplier = 1
+    purchase_multiplier = 1
+    
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+    def sale_price(self):
+        return self.base_price * self.sale_multiplier
+    def purchase_price(self):
+        return self.sale_price() - (self.purchase_multiplier * self.miles)
 
 class Car(Vehicle):
-    pass
-
+    sale_multiplier = 1.2
+    purchase_multiplier = 0.004
+    interest = 1.07
+    lease_multiplier = 1.2
 
 class Motorcycle(Vehicle):
-    pass
-
-
+    sale_multiplier = 1.1
+    purchase_multiplier = 0.009 
+    interest = 1.03
+    lease_multiplier = 1
+    
 class Truck(Vehicle):
-    pass
+    sale_multiplier = 1.6
+    purchase_multiplier = 0.02
+    interest = 1.11
+    lease_multiplier = 1.7
+ 
+
+
+"""
+my_car = Car(maker='ford')
+your_car = Car(maker='gmc')
+
+my_truck.my_method()
+
+my_car.maker"""
