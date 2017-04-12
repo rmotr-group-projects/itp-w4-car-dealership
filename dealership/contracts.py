eea3706406a667fa94e6be1b5fd2e4b4a318903b
Jon Fujita <jon@buddhistjobs.com>
2017-04-12 06:20:56 +0000

    first upload

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -4,9 +4,44 @@ class Contract(object):
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+    
+    INTEREST_RATES = {'Car': 1.07, 'Motorcycle': 1.03, 'Truck': 1.11}
+    
+    def total_value(self):
+        total_val = self.vehicle.sale_price()  \
+        + (BuyContract.INTEREST_RATES[self.vehicle.__class__.__name__]  \
+        * self.monthly_payments * self.vehicle.sale_price() / 100) 
+        
+        discount = (0 if not self.customer.is_employee() else 0.1 * total_val)
+        total_val -= discount 
+        
+        return total_val
+        
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+        
+    
+    LEASE_MULTIPLIER = {'Car': 1.2, 'Motorcycle': 1, 'Truck': 1.7}
+        
+    def total_value(self):
+        lease_multiplier = self.vehicle.sale_price() \
+        * LeaseContract.LEASE_MULTIPLIER[self.vehicle.__class__.__name__] \
+        / self.length_in_months
+        
+        total_val3 = self.vehicle.sale_price() + lease_multiplier
+        
+        return total_val3
+        
+        
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,34 @@
 class Vehicle(object):
+    """Base class for Car, Motorcycle, and Truck"""
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+    SALE_MULTIPLIERS = {'Car': 1.2, 'Motorcycle': 1.1, 'Truck': 1.6}
+    
+    PURCHASE_MULTIPLIERS = {'Car': 0.004, 'Motorcycle': 0.009, 'Truck': 0.02}
+        
+    def sale_price(self):
+        return self.base_price * Vehicle.SALE_MULTIPLIERS[self.__class__.__name__]
+    
+    def purchase_price(self):
+        return self.sale_price() - (Vehicle.PURCHASE_MULTIPLIERS[self.__class__.__name__] * self.miles)
+
 
 
 class Car(Vehicle):
+    """Subclass of Vehicle (0)"""
     pass
 
 
 class Motorcycle(Vehicle):
+    """Subclass of Vehicle (1)"""
     pass
 
 
 class Truck(Vehicle):
+    """Subclass of Vehicle (2)"""
     pass
