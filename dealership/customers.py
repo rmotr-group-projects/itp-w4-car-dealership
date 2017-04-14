38bfaffc2348fec9a60c9dcaa88cbabb196ca572
Leah <ldenham828@gmail.com>
2017-04-14 02:05:44 +0000

    project complete

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,20 @@
 class Person(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+    
+    def is_employee(self):
+        return isinstance(self, Employee)
+            
 class Customer(Person):
-    pass
-
-
+    def __init__(self, *args, **kwargs):
+        Person.__init__(self, *args, **kwargs)
+    
 class Employee(Person):
-    pass
+   
+    def __init__(self, *args, **kwargs):
+        Person.__init__(self, *args, **kwargs)
+    
+    
+    
