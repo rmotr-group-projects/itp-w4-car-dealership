6d5c6aac062da073595969fbc8cf8f4c33a51908
Dan O <dan47573@gmail.com>
2017-04-15 04:04:34 +0000

    Dan O Dealership Initial Commit

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -4,9 +4,38 @@ class Contract(object):
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+    def total_value(self):
+        if self.vehicle.__class__.__name__ == 'Car':
+            total = self.vehicle.sale_price() + (1.07 * self.monthly_payments * self.vehicle.sale_price() / 100)
+        elif self.vehicle.__class__.__name__ == 'Truck':
+            total = self.vehicle.sale_price() + (1.11 * self.monthly_payments * self.vehicle.sale_price() / 100)
+        elif self.vehicle.__class__.__name__ == 'Motorcycle':
+            total = self.vehicle.sale_price() + (1.03 * self.monthly_payments * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee():
+            return total * .9
+        else:
+            return total
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+    def total_value(self):
+        if self.vehicle.__class__.__name__ == 'Car':
+            total = self.vehicle.sale_price() + (self.vehicle.sale_price() * 1.2 / self.length_in_months)
+        elif self.vehicle.__class__.__name__ == 'Truck':
+            total = self.vehicle.sale_price() + (self.vehicle.sale_price() * 1.7 / self.length_in_months)
+        elif self.vehicle.__class__.__name__ == 'Motorcycle':
+            total = self.vehicle.sale_price() + (self.vehicle.sale_price() / self.length_in_months)
+        if self.customer.is_employee():
+            return total * .9
+        else:
+            return total
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,26 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
-
+    def sale_price(self):
+        return self.base_price * 1.2
+    def purchase_price(self):
+        return self.sale_price() - self.miles * .004
 
 class Motorcycle(Vehicle):
-    pass
-
+    def sale_price(self):
+        return self.base_price * 1.1
+    def purchase_price(self):
+        return self.sale_price() - self.miles * .009
 
 class Truck(Vehicle):
-    pass
+    def sale_price(self):
+        return self.base_price * 1.6
+    def purchase_price(self):
+        return self.sale_price() - self.miles * .02
