f50c0303857fdaaa3fc10d477271cb376a247bcc
Francisco Echeverria <fcontreras18@gmail.com>
2017-04-15 01:07:03 +0000

    Final?

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -4,9 +4,30 @@ class Contract(object):
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+        
+    def total_value(self):
+        price = self.vehicle.sale_price() + (self.vehicle.interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee() == True:
+            price *= 0.9
+        return price
+                        
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+        leaseprice = self.vehicle.sale_price() + ((self.vehicle.sale_price() * self.vehicle.lease_multiplier) / self.length_in_months)
+        if self.customer.is_employee() == True:
+            leaseprice *= 0.9
+        return leaseprice
+        
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
