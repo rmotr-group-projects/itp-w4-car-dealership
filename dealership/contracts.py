9cece396451b4fdd8145093a49e6aa4b96d07f49
Joey <joey.coakley@gmail.com>
2017-05-17 02:26:08 +0000

    OOP Dealership

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,69 @@
+from vehicles import *
+from customers import *
+
+
 class Contract(object):
-    pass
+    def __init__(self):
+        pass
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+        
+    def total_value(self):
+        if isinstance(self.vehicle, Car):
+            interest_rate = 1.07
+        elif isinstance(self.vehicle, Motorcycle):
+            interest_rate = 1.03
+        elif isinstance(self.vehicle, Truck):
+            interest_rate = 1.11
+        else:
+            return "Invalid vehicle type"
+        
+        if isinstance(self.customer, Employee):
+            #10% discount for employee
+            discount = self.customer.discount_multiplyer * (self.vehicle.sale_price() + (interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100))
+        elif isinstance(self.customer, Customer):
+            discount = self.customer.discount_multiplyer * (self.vehicle.sale_price() + (interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100))
+        else:
+            return "Invalid customer type."
+        
+        return round((self.vehicle.sale_price() + (interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100) - discount), 2)
+    
+    def monthly_value(self):
+        return round((self.total_value() / self.monthly_payments), 2)
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+        
+    def total_value(self):
+        #Lease Multiplyer
+        if isinstance(self.vehicle, Car):
+            lease_multiplyer = self.vehicle.sale_price() * 1.2 / self.length_in_months
+        elif isinstance(self.vehicle, Motorcycle):
+            lease_multiplyer = self.vehicle.sale_price() * 1 / self.length_in_months
+        elif isinstance(self.vehicle, Truck):
+            lease_multiplyer = self.vehicle.sale_price() * 1.7 / self.length_in_months
+        else:
+            return "Invalid vehicle type"
+            
+        if isinstance(self.customer, Employee):
+            #10% discount for employee
+            discount = self.customer.discount_multiplyer * (self.vehicle.sale_price() + lease_multiplyer)
+        elif isinstance(self.customer, Customer):
+            discount = self.customer.discount_multiplyer * (self.vehicle.sale_price() + lease_multiplyer)
+        else:
+            return "Invalid customer type."
+            
+        return round((self.vehicle.sale_price() + lease_multiplyer - discount), 2)
+    
+    def monthly_value(self):
+        return round((self.total_value() / self.length_in_months), 2)
+        
--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,19 @@
 class Person(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
+    def is_employee(self):
+        if isinstance(self, Employee):
+            return True
+        else:
+            return False
 
 
 class Customer(Person):
-    pass
+    discount_multiplyer = 0
 
 
 class Employee(Person):
-    pass
+    discount_multiplyer = .1
