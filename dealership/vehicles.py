0b6647fed12d1cd3c4b105370b07e2df07b23c12
sooyoungma <sooyma@gmail.com>
2017-05-19 00:43:18 +0000

    Zachary, Soo and Guillermo did it! Yay!

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,40 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
-
+    def sale_price(self):
+        # Take base price, multiply by sale modifier
+        return self.base_price * 1.2
+        
+    def purchase_price(self):
+        # Do sale price math, then calculate purchase price
+        return (self.base_price * 1.2) - (0.004 * self.miles)
+    
 
 class Motorcycle(Vehicle):
-    pass
+    def sale_price(self):
+        # Take base price, multiply by sale modifier
+        return self.base_price * 1.1
+        
+    def purchase_price(self):
+        # Do sale price math, then calculate purchase price
+        return (self.base_price * 1.1) - (0.009 * self.miles)
+    
 
 
 class Truck(Vehicle):
-    pass
+    def sale_price(self):
+        # Take base price, multiply by sale modifier
+        return self.base_price * 1.6
+        
+    def purchase_price(self):
+        # Do sale price math, then calculate purchase price
+        return (self.base_price * 1.6) - (0.02 * self.miles)
+    
+
