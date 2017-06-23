a36074a7f3b5c235ba038c2d739c27c88fce7cc3
James Curbow <curbowj@uni.edu>
2017-06-23 01:29:20 +0000

    Dealership Done

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,28 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+
+    def sale_price(self):
+        return self.base_price * self.sale_multiplier
+        
+    def purchase_price(self):
+        return self.sale_price() - (self.purchase_multiplier * self.miles)
 
 
 class Car(Vehicle):
-    pass
-
+    sale_multiplier = 1.2
+    purchase_multiplier = .004
 
 class Motorcycle(Vehicle):
-    pass
+    sale_multiplier = 1.1
+    purchase_multiplier = 0.009
 
 
 class Truck(Vehicle):
-    pass
+    sale_multiplier = 1.6
+    purchase_multiplier = 0.02
+   
