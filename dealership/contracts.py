4ac0e78b8d783918891f86022c17c83d9989663d
Tomas Espina <tespintom@icloud.com>
2017-06-23 01:06:40 +0000

    Car Dealership

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -4,9 +4,38 @@ class Contract(object):
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+    
+    def total_value(self):
+        val = self.vehicle.sale_price() + (self.monthly_payments * self.vehicle.INTEREST_RATE * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee():
+            return val - (val * .10)
+        else:
+            return val 
+        
+    def monthly_value(self):
+        
+        return self.total_value() / self.monthly_payments
+        
+    
 
 
 class LeaseContract(Contract):
+    
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+        lease_val = self.vehicle.sale_price() + (self.vehicle.sale_price() * self.vehicle.LEASE_MULTIPLIER / self.length_in_months)
+        if self.customer.is_employee():
+            return lease_val - (lease_val * .10)
+        else:
+            return lease_val
+            
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
+
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,39 @@
 class Vehicle(object):
+    
+    
+   
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+    def sale_price(self):
+        return self.base_price * self.SALE_MULTIPLIER
+    
+    def purchase_price(self):
+        return self.sale_price() - (self.PURCHASE_MULTIPLIER * self.miles)    
 
 
 class Car(Vehicle):
-    pass
-
+    SALE_MULTIPLIER = 1.2
+    PURCHASE_MULTIPLIER = 0.004
+    INTEREST_RATE = 1.07
+    LEASE_MULTIPLIER = 1.2
+        
 
 class Motorcycle(Vehicle):
-    pass
-
+    SALE_MULTIPLIER = 1.1
+    PURCHASE_MULTIPLIER = 0.009
+    INTEREST_RATE = 1.03
+    LEASE_MULTIPLIER = 1
+    
 
 class Truck(Vehicle):
-    pass
+    SALE_MULTIPLIER = 1.6
+    PURCHASE_MULTIPLIER = 0.02
+    INTEREST_RATE = 1.11
+    LEASE_MULTIPLIER = 1.7
+    
+    
