69811bd307abc8145a913c89f4916108ee689cec
Brett Wildblood <tensai2bai@gmail.com>
2017-08-23 23:22:25 +0000

    All tests passing

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,39 @@
 class Contract(object):
-    pass
-
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
+    
+    
+    def total_value(self):
+        pass
+    
+    def monthly_value(self):
+        pass
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract,self).__init__(vehicle, customer)
+        self.monthly_payments = monthly_payments
 
+    def total_value(self):
+        value = self.vehicle.sale_price() + (self.vehicle.I * self.monthly_payments * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee():
+            value -= value * .1 #10% discout for employees
+        return value
+    
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+
+    def total_value(self):
+        value = self.vehicle.sale_price() + (self.vehicle.sale_price() * self.vehicle.L / self.length_in_months)
+        if self.customer.is_employee():
+            value -= value * .1 #10% discout for employees
+        return value
+        
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
