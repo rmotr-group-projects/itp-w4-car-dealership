3e689b37dee7f923ed900c59f772c70b174f1074
Reina Chano Murray <r.chano@gmail.com>
2017-08-24 00:29:21 +0000

    all tests passing

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -3,17 +3,26 @@ class Contract(object):
         self.vehicle = vehicle
         self.customer = customer
     
-    def total_value():  #total value of the contract to pay by a customer
-        pass
     
-    def monthly_value(): # amount of money that a customer is supposed to pay monthly
-        pass
-
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
         super(BuyContract, self).__init__(vehicle, customer)
         
         self.monthly_payments = monthly_payments
+
+    def total_value(self):  #total value of the contract to pay by a customer
+        total_value = self.vehicle.sale_price() + ((self.vehicle.INTEREST_RATE * self.monthly_payments * self.vehicle.sale_price()) / 100)
+        
+        if self.customer.is_employee() is True:
+            total_value = total_value * 0.9
+            return total_value
+        else:
+            return total_value
+    
+    
+    def monthly_value(self): # amount of money that a customer is supposed to pay monthly
+        monthly_value = BuyContract.total_value(self) / self.monthly_payments
+        return monthly_value
 
 
 class LeaseContract(Contract):
@@ -21,3 +30,17 @@ class LeaseContract(Contract):
         super(LeaseContract, self).__init__(vehicle, customer)
         
         self.length_in_months = length_in_months
+
+    def total_value(self):  
+        lease_multiplier = (self.vehicle.sale_price() * self.vehicle.LEASE_MULT) / self.length_in_months
+        total_value = self.vehicle.sale_price() + lease_multiplier 
+        
+        if self.customer.is_employee() is True:
+            total_value = total_value * 0.9
+            return total_value
+        else:
+            return total_value
+
+    def monthly_value(self):
+        monthly_value = LeaseContract.total_value(self) / self.length_in_months
+        return monthly_value
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -12,21 +12,21 @@ class Vehicle(object):
     def purchase_price(self):
         return (self.sale_price()) - (self.miles * self.PURCHASE_MULT)
         
- 
- 
- 
+        
 class Car(Vehicle):
     SALE_MULT = 1.2
     PURCHASE_MULT = 0.004
     INTEREST_RATE = 1.07
+    LEASE_MULT = 1.2
     
 class Motorcycle(Vehicle):
     SALE_MULT = 1.1
     PURCHASE_MULT = 0.009
     INTEREST_RATE = 1.03
-
+    LEASE_MULT = 1
 
 class Truck(Vehicle):
     SALE_MULT = 1.6
     PURCHASE_MULT = 0.02
     INTEREST_RATE = 1.11
+    LEASE_MULT = 1.7
