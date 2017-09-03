c91cab07ff8ef95471508bc0c78ca5afbf519b74
Sara Amin <saraaamin@gmail.com>
2017-09-03 13:27:52 +0000

    Project completed and all tests passed

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,34 @@
 class Vehicle(object):
+    
+    # defining the init funtion that initializes all the variables values of created object
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+    def sale_price(self):
+        return self.base_price * self.S
+    
+    def purchase_price(self):
+        return (self.sale_price() - self.P * self.miles)
 
 class Car(Vehicle):
-    pass
-
-
+    S = 1.2
+    P = 0.004
+    I = 1.07
+    LEASE_MULTI = 1.2
+    
 class Motorcycle(Vehicle):
-    pass
-
-
+    S = 1.1
+    P = 0.009
+    I = 1.03
+    LEASE_MULTI = 1.0
+    
 class Truck(Vehicle):
-    pass
+    S = 1.6
+    P = 0.02    
+    I = 1.11
+    LEASE_MULTI = 1.7
+    
