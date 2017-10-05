c772334ccda940afb7ea69a15b66f451e8dd5cdf
Alicia Stillwell <alicia.stillwell@gmail.com>
2017-10-05 16:24:59 +0000

    Cleaned up total value calculations in contracts

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -9,11 +9,8 @@ class BuyContract(Contract):
         self.monthly_payments = monthly_payments
     
     def total_value(self):
-        if self.customer.is_employee():
-            discount = 1 - self.customer.discount
-        else:
-            discount = 1
-        return discount * (self.vehicle.sale_price() + (self.vehicle.interest_rate*self.monthly_payments*self.vehicle.sale_price()/100))
+        interest = self.vehicle.interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100
+        return (1 - (self.customer.discount if self.customer.is_employee() else 0)) * (self.vehicle.sale_price() + interest)
     
     def monthly_value(self):
         return self.total_value() / self.monthly_payments
@@ -26,12 +23,8 @@ class LeaseContract(Contract):
         self.length_in_months = length_in_months
 
     def total_value(self):
-        if self.customer.is_employee():
-            discount = 1 - self.customer.discount
-        else:
-            discount = 1
-        lease_multiplier = (self.vehicle.sale_price()*self.vehicle.lease_multiplier)/self.length_in_months
-        return discount*(self.vehicle.sale_price()+lease_multiplier)
+        lease_cost = (self.vehicle.sale_price() * self.vehicle.lease_multiplier) / self.length_in_months
+        return (1 - (self.customer.discount if self.customer.is_employee() else 0)) * (self.vehicle.sale_price() + lease_cost)
     
     def monthly_value(self):
         return self.total_value() / self.length_in_months
