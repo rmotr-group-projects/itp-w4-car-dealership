872a8009ae020d5f8ca4f84ab0ba9af354eacc9d
Alicia Stillwell <alicia.stillwell@gmail.com>
2017-10-05 16:27:23 +0000

    Increase readability of total value calculation

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -10,7 +10,8 @@ class BuyContract(Contract):
     
     def total_value(self):
         interest = self.vehicle.interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100
-        return (1 - (self.customer.discount if self.customer.is_employee() else 0)) * (self.vehicle.sale_price() + interest)
+        discount_multiplier = 1 - (self.customer.discount if self.customer.is_employee() else 0)
+        return discount_multiplier * (self.vehicle.sale_price() + interest)
     
     def monthly_value(self):
         return self.total_value() / self.monthly_payments
@@ -24,7 +25,8 @@ class LeaseContract(Contract):
 
     def total_value(self):
         lease_cost = (self.vehicle.sale_price() * self.vehicle.lease_multiplier) / self.length_in_months
-        return (1 - (self.customer.discount if self.customer.is_employee() else 0)) * (self.vehicle.sale_price() + lease_cost)
+        discount_multiplier = 1 - (self.customer.discount if self.customer.is_employee() else 0)
+        return discount_multiplier * (self.vehicle.sale_price() + lease_cost)
     
     def monthly_value(self):
         return self.total_value() / self.length_in_months
