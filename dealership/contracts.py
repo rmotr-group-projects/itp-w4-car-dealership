a8d4287dc4fa7b40013c718653e11a4e86bcb290
Alicia Stillwell <alicia.stillwell@gmail.com>
2017-10-05 03:44:04 +0000

    Added basic functionality

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -4,9 +4,34 @@ class Contract(object):
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+    
+    def total_value(self):
+        if self.customer.is_employee():
+            discount = 1 - self.customer.discount
+        else:
+            discount = 1
+        return discount * (self.vehicle.sale_price() + (self.vehicle.interest_rate*self.monthly_payments*self.vehicle.sale_price()/100))
+    
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+
+    def total_value(self):
+        if self.customer.is_employee():
+            discount = 1 - self.customer.discount
+        else:
+            discount = 1
+        lease_multiplier = (self.vehicle.sale_price()*self.vehicle.lease_multiplier)/self.length_in_months
+        return discount*(self.vehicle.sale_price()+lease_multiplier)
+    
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,39 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
+    def sale_price(self):
+        return self.base_price * self.sale_multiplier
+    
+    def purchase_price(self):
+        return self.sale_price() - (self.purchase_multiplier * self.miles)
 
 class Car(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        self.sale_multiplier = 1.2
+        self.purchase_multiplier = 0.004
+        self.interest_rate = 1.07
+        self.lease_multiplier = 1.2
+        super(Car, self).__init__(maker, model, year, base_price, miles)
 
 
 class Motorcycle(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        self.sale_multiplier = 1.1
+        self.purchase_multiplier = 0.009
+        self.interest_rate = 1.03
+        self.lease_multiplier = 1
+        super(Motorcycle, self).__init__(maker, model, year, base_price, miles)
 
 
 class Truck(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        self.sale_multiplier = 1.6
+        self.purchase_multiplier = 0.02
+        self.interest_rate = 1.11
+        self.lease_multiplier = 1.7
+        super(Truck, self).__init__(maker, model, year, base_price, miles)
