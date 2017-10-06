aea32ede6f6205567fed5eb4b9f2affe724b63c1
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-06 00:36:36 +0000

    Working Customer.py

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,7 +1,15 @@
 class Person(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+    
+    @classmethod
+    def is_employee(cls):
+        if cls is Employee:
+            return True
+        else:
+            return False
 
 class Customer(Person):
     pass
