eff988d310a57b747174f0aeddc0f6d59719e9f7
laurauzcategui <laura.uzcateguij@gmail.com>
2017-10-09 23:24:07 +0100

    Working in the project - Contracts and Customer

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,19 +1,48 @@
 class Contract(object):
-    def __init__(self, customer, vehicle): 
-        self.customer = customer 
-        self.vehicle = vehicle 
-
-    def total_value(self):
-        pass
-    
-    def monthly_value(self):
-        pass 
+    def __init__(self, customer, vehicle):
+        self.customer = customer
+        self.vehicle = vehicle
+        self.interest = {
+            'Car': 1.07,
+            'Motorcycle': 1.03,
+            'Truck': 1.11
+        }
+        if self.customer.is_employee():
+            self.discount = 0.10
+        else:
+            self.discount = 0.0
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        Contract.__init__(self, customer, vehicle)
+        self.monthly_payments = monthly_payments
 
+    def total_value(self):
+        class_type = type(self.vehicle).__name__
+        sale_price = self.vehicle.sale_price()
+
+        prev_total = sale_price + \
+        (self.interest[class_type] * self.monthly_payments * sale_price / 100)
+        return prev_total - (prev_total * self.discount)
+
+
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        Contract.__init__(self, customer, vehicle)
+        self.length_in_months = length_in_months
+        self.multiplier = {
+            'Car': 1.2,
+            'Motorcycle': 1,
+            'Truck': 1.7
+        }
+
+    def total_value(self):
+        class_type = type(self.vehicle).__name__
+        multiplier = self.vehicle.sale_price() * self.multiplier[class_type] / self.length_in_months
+        return self.vehicle.sale_price() + multiplier - self.discount
+
+    def monthly_value(self):
+        self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,34 +1,39 @@
 class Vehicle(object):
-    def __init__(self, maker, model, year, base_price, miles):
+    def __init__(self, maker, model, year, base_price, miles, multiplier):
         self.maker = maker
-        self.model = model 
-        self.year = year 
+        self.model = model
+        self.year = year
         self.base_price= base_price
         self.miles = miles
+        self.multiplier = multiplier
 
+    def sale_price(self):
+        return self.base_price * self.multiplier['sale']
+
+    def purchase_price(self):
+        return self.sale_price() - (self.multiplier['purchase'] * self.miles)
 
 class Car(Vehicle):
     def __init__(self, maker, model, year, base_price, miles):
-        Vehicle.__init__(self, maker, model, year, base_price, miles)
-        self.multiplier = { 
-            'sale': 1.2,
+        car_multiplier = {
+            'sale': 1.2 ,
             'purchase': 0.004
         }
- 
+        Vehicle.__init__(self, maker, model, year, base_price, miles, car_multiplier)
 
 class Motorcycle(Vehicle):
     def __init__(self, maker, model, year, base_price, miles):
-        Vehicle.__init__(self, maker, model, year, base_price, miles)
-        self.multiplier = { 
+        moto_multiplier = {
             'sale': 1.1,
             'purchase': 0.009
         }
+        Vehicle.__init__(self, maker, model, year, base_price, miles,moto_multiplier)
 
 
 class Truck(Vehicle):
     def __init__(self, maker, model, year, base_price, miles):
-        Vehicle.__init__(self, maker, model, year, base_price, miles)
-        self.multiplier = { 
+        truck_multiplier = {
             'sale': 1.6,
             'purchase': 0.02
         }
+        Vehicle.__init__(self, maker, model, year, base_price, miles, truck_multiplier)
