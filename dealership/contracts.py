1dc5c5ddf55cbb67d37a4010937a1d5ac1ae0145
courtneypattison <courtney@courtneypattison.com>
2017-10-09 09:45:32 -0700

    Implemented contracts.py

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,33 @@
 class Contract(object):
-    pass
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
+
+    def total_value(self):
+        raise NotImplementedError("total_value() not implemented")
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract, self).__init__(vehicle, customer)
+        self.monthly_payments = monthly_payments
+
+    def total_value(self):
+        value = self.vehicle.sale_price() + (self.vehicle.interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100)
+        return value if not self.customer.is_employee() else value - (value * 0.1)
+
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments if self.monthly_payments != 0 else 0
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+
+    def total_value(self):
+        value = self.vehicle.sale_price() + self.vehicle.lease_multiplier(self.length_in_months)
+        return value if not self.customer.is_employee() else value - (value * 0.1)
+
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months if self.length_in_months != 0 else 0
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -7,6 +7,7 @@ class Vehicle(object):
         self.miles = miles
         self.sale_multiplier = 1
         self.purchase_multiplier = 1
+        self.interest_rate = 1
 
     def sale_price(self):
         return self.base_price * self.sale_multiplier
@@ -14,12 +15,19 @@ class Vehicle(object):
     def purchase_price(self):
         return self.sale_price() - (self.purchase_multiplier * self.miles)
 
+    def lease_multiplier(self, length_in_months):
+        raise NotImplementedError("lease_multiplier(length_in_months) not implemented")
+
 
 class Car(Vehicle):
     def __init__(self, maker, model, year, base_price, miles):
         super(Car, self).__init__(maker, model, year, base_price, miles)
         self.sale_multiplier = 1.2
         self.purchase_multiplier = 0.004
+        self.interest_rate = 1.07
+
+    def lease_multiplier(self, length_in_months):
+        return self.sale_price() * 1.2 / length_in_months if length_in_months != 0 else 0
 
 
 class Motorcycle(Vehicle):
@@ -27,6 +35,10 @@ class Motorcycle(Vehicle):
         super(Motorcycle, self).__init__(maker, model, year, base_price, miles)
         self.sale_multiplier = 1.1
         self.purchase_multiplier = 0.009
+        self.interest_rate = 1.03
+
+    def lease_multiplier(self, length_in_months):
+        return self.sale_price() / length_in_months if length_in_months != 0 else 0
 
 
 class Truck(Vehicle):
@@ -34,3 +46,7 @@ class Truck(Vehicle):
         super(Truck, self).__init__(maker, model, year, base_price, miles)
         self.sale_multiplier = 1.6
         self.purchase_multiplier = 0.02
+        self.interest_rate = 1.11
+
+    def lease_multiplier(self, length_in_months):
+        return self.sale_price() * 1.7 / length_in_months if length_in_months != 0 else 0
