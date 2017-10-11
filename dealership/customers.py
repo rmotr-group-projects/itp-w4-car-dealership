50e6e8afa3ac86210c5a23df140717a35f829092
agerista <amandacrawfordcodes@gmail.com>
2017-10-11 03:50:44 +0000

    Employee and Customer classes

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,23 @@
 class Person(object):
-    def __init__(self, first_name, last_name, email):
-        pass
+    def __init__(self, first_name, last_name, email, discount=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = "{}@example.com".format(self.first_name).lower()
+        self.discount = None
+
+    def is_employee(self):
+        return False
 
 
 class Customer(Person):
-    pass
+    def __init__(self, first_name, last_name, email):
+        super(Customer, self).__init__(first_name, last_name, email)
 
 
 class Employee(Person):
-    pass
+    def __init__(self, first_name, last_name, email, discount=0.10):
+        super(Employee, self).__init__(first_name, last_name, email)
+        self.discount = discount
+
+    def is_employee(self):
+        return True
