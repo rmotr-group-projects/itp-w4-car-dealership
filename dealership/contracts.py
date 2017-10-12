37b9247903cbec4a15702345a9ef2d99b23371e6
naveeda11 <naveed.agboatwala11@gmail.com>
2017-10-12 16:55:33 +0000

    1.0 version of OOP Car Dealership

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,46 @@
 class Contract(object):
-    pass
-
+    employee_discount = .1
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
+        
+    def total_value(self):
+        raise NotImplementedError()
+    def monthly_value(self):
+        raise NotImplementedError()
+        
+    def determine_final_price(self, undiscounted_value):
+        if self.customer.is_employee():
+            return undiscounted_value * (1 - self.__class__.employee_discount)
+        else:
+            return undiscounted_value
+        
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
+        super(BuyContract, self).__init__(vehicle, customer)
+        self.monthly_payments = monthly_payments
+    def total_value(self):
+        undiscounted_value = self.vehicle.sale_price() + (self.vehicle.INTEREST_RATE * self.monthly_payments * self.vehicle.sale_price() / 100)
+        return self.determine_final_price(undiscounted_value)
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+        undiscounted_value = self.vehicle.sale_price() + self.lease_multiplier()
+        return self.determine_final_price(undiscounted_value)
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
+    def lease_multiplier(self):
+        return self.vehicle.sale_price() * self.vehicle.LEASE_MULTIPLIER / self.length_in_months
+        
+        
+        
+    
+
+
