9aceebd7c0ca9cd300b1aef7acf8937a43308b4e
Dorota <niezborala.dorota@gmail.com>
2017-10-12 17:37:50 +0000

    Solution properly working

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,43 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+    def sale_price(self, base_price, x):
+        return self.base_price
+        
+    def purchase_price(self, base_price, x):
+        return self.base_price * x
+        
 
 class Car(Vehicle):
-    pass
 
+    def sale_price(self):
+        return self.base_price * 1.2
+        
+    def purchase_price(self):
+        s_price = self.sale_price()
+        return s_price - (0.004 * self.miles)
+        
 
 class Motorcycle(Vehicle):
-    pass
 
+    def sale_price(self):
+        return self.base_price * 1.1
+
+    def purchase_price(self):
+        s_price = self.sale_price()
+        return s_price - (0.009 * self.miles)
 
 class Truck(Vehicle):
-    pass
+
+    def sale_price(self):
+        return self.base_price * 1.6
+        
+    def purchase_price(self):
+        s_price = self.sale_price()
+        return s_price - (0.02 * self.miles)
+
