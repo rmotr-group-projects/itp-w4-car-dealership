62b94014b4d928bd086ecf5105d0284465e2d51d
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-13 12:21:01 +0000

    made discount class attributes and is_employee in each class

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,19 +1,23 @@
 class Person(object):
+    # Put discount in 
+    discount = 0
     
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-    
-    # Made this a classmethod so both Customers and Employees could use it as well as to check the cls
-    # Don't know if this was the right way to do it. might be able to use self.__class__.__name__
-    @classmethod
-    def is_employee(cls):
-        return cls == Employee
 
+    def is_employee(self):
+        raise NotImplementedError()
+    
 # Put discounts here because they will be static for every instance of these classes aka Class Attributes 
 class Customer(Person):
-    discount = 0
+    def is_employee(self):
+        return False
+    
 
 class Employee(Person):
     discount = .1
+    
+    def is_employee(self):
+        return True
