ce54312a5cc0b7b5da39eaa359ea625e526daaf6
Tae Kim <taehnkim@gmail.com>
2017-10-15 17:12:02 +0000

    save changes

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,23 @@
-class Contract(object):
-    pass
+# from dealership.vehicles import Vehicle as veh
+# from dealership.customers import Person
 
+class Contract(object):
+    def __init__(self, vehicle, customer, monthly_payments):
+        super(Contract, self).__init__()
+        self.monthly_payments = monthly_payments
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract, self).__init__(vehicle, customer, monthly_payments)
+        
+    def total_value(self):
+        return self.sale_price() + (self.interest_rate() * self.monthly_payments * self.sale_price() / 100) #- (discount if employee)
+    
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,58 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
+    # Define purchase multipliers
+    def __init__(self, maker, model, year, base_price, miles):
+        # Inherit vehicle attributes
+        super(Car, self).__init__(maker, model, year, base_price, miles)
+
+    def sale_price(self):
+        s = 1.2
+        return self.base_price * s
+        
+    def purchase_price(self):
+        p = 0.004
+        return self.sale_price() - (p * self.miles)
+    
+    def interest_rate(self):
+        return 1.07
 
 
 class Motorcycle(Vehicle):
-    pass
-
+    def __init__(self, maker, model, year, base_price, miles):
+        # Inherit vehicle attributes
+        super(Motorcycle, self).__init__(maker, model, year, base_price, miles)
+        
+    def sale_price(self):
+        s = 1.1
+        return self.base_price * s
+        
+    def purchase_price(self):
+        p = 0.009
+        return self.sale_price() - (p * self.miles)
+    
+    def interest_rate(self):
+        return 1.03
 
 class Truck(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        # Inherit vehicle attributes
+        super(Truck, self).__init__(maker, model, year, base_price, miles)
+        
+    def sale_price(self):
+        s = 1.6
+        return self.base_price * s
+        
+    def purchase_price(self):
+        p = 0.02
+        return self.sale_price() - (p * self.miles)
+        
+    def interest_rate(self):
+        return 1.11
