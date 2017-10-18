9b6549458d522862c1440f74b40f5523bc602d0b
Erika Boren <eboren@gmail.com>
2017-10-18 00:51:35 +0000

    refactor vehicles code

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -5,26 +5,44 @@ class Vehicle(object):
         self.year = year
         self.base_price = base_price
         self.miles = miles
-
-class Car(Vehicle):
+        
     def sale_price(self):
-        return self.base_price * 1.2
+        return self.base_price * self.purchase_multiplier
         
     def purchase_price(self):
-        return self.sale_price() - (0.004 * self.miles)
+        return self.sale_price() - (self.sale_multiplier * self.miles)    
+
+class Car(Vehicle):
+    interest = 1.07
+    sale_multiplier = .004
+    purchase_multiplier = 1.2
+    
+    # def sale_price(self):
+    #     return self.base_price * 1.2
+        
+    # def purchase_price(self):
+    #     return self.sale_price() - (0.004 * self.miles)
 
 
 class Motorcycle(Vehicle):
-    def sale_price(self):
-        return self.base_price * 1.1
+    interest = 1.03
+    sale_multiplier = .009
+    purchase_multiplier = 1.1
+    
+    # def sale_price(self):
+    #     return self.base_price * 1.1
         
-    def purchase_price(self):
-        return self.sale_price() - (0.009 * self.miles)   
+    # def purchase_price(self):
+    #     return self.sale_price() - (0.009 * self.miles)   
 
 
 class Truck(Vehicle):
-    def sale_price(self):
-        return self.base_price * 1.6
+    interest = 1.11
+    sale_multiplier = .02
+    purchase_multiplier = 1.6
+    
+    # def sale_price(self):
+    #     return self.base_price * 1.6
         
-    def purchase_price(self):
-        return self.sale_price() - (0.02 * self.miles)
+    # def purchase_price(self):
+    #     return self.sale_price() - (0.02 * self.miles)
