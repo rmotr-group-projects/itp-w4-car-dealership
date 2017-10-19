cabda2bc44c2266d74efcbcb3bd80c551749b8c3
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-19 17:53:10 +0000

    made vehicles pep8 compliant

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -5,24 +5,28 @@ class Vehicle(object):
         self.year = year
         self.base_price = base_price
         self.miles = miles
-    
+
     def sale_price(self):
         return self.base_price * self.sale_price_multiplier
-    
+
     def purchase_price(self):
-        return self.sale_price() - (self.purchase_price_multiplier * self.miles)
-        
+        return self.sale_price() - (
+            self.purchase_price_multiplier * self.miles)
+
+
 class Car(Vehicle):
         sale_price_multiplier = 1.2
         purchase_price_multiplier = .004
         interest = 1.07
         lease_mulitiplier = 1.2
 
+
 class Motorcycle(Vehicle):
         sale_price_multiplier = 1.1
         purchase_price_multiplier = .009
         interest = 1.03
         lease_mulitiplier = 1
+
 
 class Truck(Vehicle):
         sale_price_multiplier = 1.6
