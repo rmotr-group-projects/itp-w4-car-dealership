f508c1257493ced7753a6f3ada827d22ecb2eb3f
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-19 17:24:07 +0000

    made code pep8 compliant

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,7 +1,7 @@
 class Person(object):
-    # Put discount in 
+    # Put discount in
     discount = 0
-    
+
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
@@ -9,15 +9,17 @@ class Person(object):
 
     def is_employee(self):
         raise NotImplementedError()
-    
-# Put discounts here because they will be static for every instance of these classes aka Class Attributes 
+
+
 class Customer(Person):
     def is_employee(self):
         return False
-    
+
 
 class Employee(Person):
+    # Put discount here because they will be static for every instance of these
+    # classes aka Class Attributes
     discount = .1
-    
+
     def is_employee(self):
         return True
