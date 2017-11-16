181d34db9d1ea3365ebaa82289f06a27370e832a
Santiago Basulto <santiago.basulto@gmail.com>
2017-11-15 22:14:45 -0300

    Updated tests to py.test syntax

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -1,93 +1,79 @@
-import unittest
+from pytest import approx
 
 from dealership.contracts import BuyContract, LeaseContract
 from dealership.customers import Customer, Employee
 from dealership.vehicles import Car, Truck, Motorcycle
 
 
-class BaseContractTestCase(unittest.TestCase):
-    def setUp(self):
-        self.car = Car(maker='Ford', model='Mustang', year=2005,
-                       base_price=18000, miles=31000)
+car = Car(
+    maker='Ford', model='Mustang', year=2005, base_price=18000, miles=31000)
 
-        self.truck = Truck(maker='Chevrolet', model='Silverado', year=2014,
-                           base_price=29000, miles=3000)
+truck = Truck(
+    maker='Chevrolet', model='Silverado', year=2014,
+    base_price=29000, miles=3000)
 
-        self.bike = Motorcycle(maker='Ducati', model='Monster',
-                               year=2016, base_price=18000, miles=700)
+bike = Motorcycle(
+    maker='Ducati', model='Monster', year=2016, base_price=18000, miles=700)
 
-        self.customer = Customer('John', 'Doe', 'john@example.com')
-        self.employee = Employee('Jane', 'Doe', 'jane@example.com')
-
-
-class BuyContractTestCase(BaseContractTestCase):
-    def test_buy_contract_total_value_with_customer(self):
-        car_contract = BuyContract(
-            vehicle=self.car, customer=self.customer, monthly_payments=6)
-
-        self.assertAlmostEqual(car_contract.total_value(), 22986.72, places=3)
-        self.assertAlmostEqual(car_contract.monthly_value(), 3831.12, places=3)
-
-        truck_contract = BuyContract(
-            vehicle=self.truck, customer=self.customer, monthly_payments=12)
-
-        self.assertAlmostEqual(
-            truck_contract.total_value(), 52580.47, places=1)
-        self.assertAlmostEqual(
-            truck_contract.monthly_value(), 4381.70, places=1)
-
-        bike_contract = BuyContract(
-            vehicle=self.bike, customer=self.customer, monthly_payments=36)
-
-        self.assertAlmostEqual(
-            bike_contract.total_value(), 27141.84, places=2)
-        self.assertAlmostEqual(
-            bike_contract.monthly_value(), 753.94, places=2)
-
-    def test_buy_contract_total_value_with_employee(self):
-        car_contract = BuyContract(
-            vehicle=self.car, customer=self.employee, monthly_payments=6)
-
-        self.assertAlmostEqual(car_contract.total_value(), 20688.04, places=1)
-        self.assertAlmostEqual(car_contract.monthly_value(), 3448, places=1)
-
-        truck_contract = BuyContract(
-            vehicle=self.truck, customer=self.employee, monthly_payments=12)
-
-        self.assertAlmostEqual(
-            truck_contract.total_value(), 47322.43, places=2)
-        self.assertAlmostEqual(
-            truck_contract.monthly_value(), 3943.53, places=1)
-
-        bike_contract = BuyContract(
-            vehicle=self.bike, customer=self.employee, monthly_payments=36)
-
-        self.assertAlmostEqual(
-            bike_contract.total_value(), 24427.65, places=1)
-        self.assertAlmostEqual(
-            bike_contract.monthly_value(), 678.54, places=1)
+customer = Customer('John', 'Doe', 'john@example.com')
+employee = Employee('Jane', 'Doe', 'jane@example.com')
 
 
-class LeaseContractTestCase(BaseContractTestCase):
-    def test_lease_contract_total_value_with_customer(self):
-        car_contract = LeaseContract(
-            vehicle=self.car, customer=self.customer, length_in_months=12)
+def test_buy_contract_total_value_with_customer():
+    car_contract = BuyContract(
+        vehicle=car, customer=customer, monthly_payments=6)
 
-        self.assertAlmostEqual(car_contract.total_value(), 23760.0, places=3)
-        self.assertAlmostEqual(car_contract.monthly_value(), 1980.0, places=3)
+    assert car_contract.total_value() == approx(22986.72, rel=1e-5)
+    assert car_contract.monthly_value() == approx(3831.12, rel=1e-5)
 
-        truck_contract = LeaseContract(
-            vehicle=self.truck, customer=self.customer, length_in_months=24)
+    truck_contract = BuyContract(
+        vehicle=truck, customer=customer, monthly_payments=12)
 
-        self.assertAlmostEqual(
-            truck_contract.total_value(), 49686.66, places=1)
-        self.assertAlmostEqual(
-            truck_contract.monthly_value(), 2070.27, places=1)
+    assert truck_contract.total_value() == approx(52580.47, rel=1e-5)
+    assert truck_contract.monthly_value() == approx(4381.70, rel=1e-5)
 
-        bike_contract = LeaseContract(
-            vehicle=self.bike, customer=self.customer, length_in_months=36)
+    bike_contract = BuyContract(
+        vehicle=bike, customer=customer, monthly_payments=36)
 
-        self.assertAlmostEqual(
-            bike_contract.total_value(), 20350, places=2)
-        self.assertAlmostEqual(
-            bike_contract.monthly_value(), 565.27, places=1)
+    assert bike_contract.total_value() == approx(27141.84, rel=1e-5)
+    assert bike_contract.monthly_value() == approx(753.94, rel=1e-5)
+
+
+def test_buy_contract_total_value_with_employee():
+    car_contract = BuyContract(
+        vehicle=car, customer=employee, monthly_payments=6)
+
+    car_contract.total_value() == approx(20688.04, rel=1e-5)
+    car_contract.monthly_value() == approx(3448, rel=1e-5)
+
+    truck_contract = BuyContract(
+        vehicle=truck, customer=employee, monthly_payments=12)
+
+    assert truck_contract.total_value() == approx(47322.43, rel=1e-5)
+    assert truck_contract.monthly_value() == approx(3943.53, rel=1e-5)
+
+    bike_contract = BuyContract(
+        vehicle=bike, customer=employee, monthly_payments=36)
+
+    assert bike_contract.total_value() == approx(24427.65, rel=1e-5)
+    assert bike_contract.monthly_value() == approx(678.54, rel=1e-5)
+
+
+def test_lease_contract_total_value_with_customer():
+    car_contract = LeaseContract(
+        vehicle=car, customer=customer, length_in_months=12)
+
+    assert car_contract.total_value() == approx(23760.0, rel=1e-4)
+    assert car_contract.monthly_value() == approx(1980.0, rel=1e-4)
+
+    truck_contract = LeaseContract(
+        vehicle=truck, customer=customer, length_in_months=24)
+
+    assert truck_contract.total_value() == approx(49686.66, rel=1e-4)
+    assert truck_contract.monthly_value() == approx(2070.27, rel=1e-4)
+
+    bike_contract = LeaseContract(
+        vehicle=bike, customer=customer, length_in_months=36)
+
+    assert bike_contract.total_value() == approx(20350, rel=1e-4)
+    assert bike_contract.monthly_value() == approx(565.27, rel=1e-4)
