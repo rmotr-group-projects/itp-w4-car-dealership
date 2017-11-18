392268ae5396720d758d7a8b2d50696a80cbe46e
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-11-18 01:48:51 +0000

    Add methods to Contract classes

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,44 @@
 class Contract(object):
-    pass
+    def total_value():
+        raise NotImplementedError
+    
+    def monthly_value():
+        raise NotImplementedError
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+
+    def total_value(self):
+        total_value = self.vehicle.sale_price() + \
+            (self.vehicle.monthly_interest_rate * \
+            self.monthly_payments * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee():
+            total_value -= (total_value * 0.1)
+        return total_value
+    
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
+        
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+
+    def total_value(self):
+        total_value = self.vehicle.sale_price() + \
+        (self.vehicle.sale_price() * self.vehicle.lease_multiplier / self.length_in_months)
+        if self.customer.is_employee():
+            total_value -= (total_value * 0.1)
+        return total_value
+    
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
+
+
