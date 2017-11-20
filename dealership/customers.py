6735cb1722f21ab18d20eff49c9f076d364a4614
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-11-19 19:51:50 -0500

    first commit

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,5 +1,5 @@
 class Person(object):
-    
+
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
@@ -7,22 +7,22 @@ class Person(object):
 
 
 class Customer(Person):
-    
+
     def __init__(self, first_name, last_name, email):
-        
+
         Person.__init__(self, first_name, last_name, email)
-        
+
     def is_employee(self):
-        
+
         return False
 
 
 class Employee(Person):
-    
+
     def __init__(self, first_name, last_name, email):
-        
+
         Person.__init__(self, first_name, last_name, email)
-        
+
     def is_employee(self):
-        
+
         return True
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,52 @@
 class Vehicle(object):
+
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
+
+    def __init__(self, maker, model, year, base_price, miles):
+
+        Vehicle.__init__(self, maker, model, year, base_price, miles)
+
+    def sale_price(self):
+
+        return self.base_price * 1.2
+
+
+# STOPEED HERE, implement purchase price for others and test
+    def purchase_price(self):
+        """The price the dealership will pay a customer to buy her/his vehicle"""
+
+        return self.sale_price() - (0.004 * self.miles)
+
+
+
 
 
 class Motorcycle(Vehicle):
-    pass
+
+    def __init__(self, maker, model, year, base_price, miles):
+
+        Motorcycle.__init__(self, maker, model, year, base_price, miles)
+
+    def sale_price(self):
+
+        return self.base_price * 1.1
 
 
 class Truck(Vehicle):
-    pass
+
+    def __init__(self, maker, model, year, base_price, miles):
+
+        Truck.__init__(self, maker, model, year, base_price, miles)
+
+    def sale_price(self):
+
+        return self.base_price * 1.6
+
