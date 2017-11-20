1b41a72abe5d406fd438d24410041403e45296c6
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-11-19 21:37:17 -0500

    change format of methods

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -7,17 +7,23 @@ class Vehicle(object):
         self.base_price = base_price
         self.miles = miles
 
+    def sale_price(self):
+        return self.base_price * self.get_sale_multiplier()
+
+    def purchase_price(self):
+        return self.sale_price() - (self.get_purchase_multiplier() * self.miles)
+
 
 class Car(Vehicle):
 
     def __init__(self, maker, model, year, base_price, miles):
         Vehicle.__init__(self, maker, model, year, base_price, miles)
 
-    def sale_price(self):
-        return self.base_price * 1.2
+    def get_sale_multiplier(self):
+        return 1.2
 
-    def purchase_price(self):
-        return self.sale_price() - (0.004 * self.miles)
+    def get_purchase_multiplier(self):
+        return 0.004
 
 
 class Motorcycle(Vehicle):
@@ -25,11 +31,11 @@ class Motorcycle(Vehicle):
     def __init__(self, maker, model, year, base_price, miles):
         Motorcycle.__init__(self, maker, model, year, base_price, miles)
 
-    def sale_price(self):
-        return self.base_price * 1.1
+    def get_sale_multiplier(self):
+        return 1.1
 
-    def purchase_price(self):
-        return self.sale_price() - (0.009 * self.miles)
+    def get_purchase_multiplier(self):
+        return 0.009
 
 
 class Truck(Vehicle):
@@ -37,9 +43,5 @@ class Truck(Vehicle):
     def __init__(self, maker, model, year, base_price, miles):
         Truck.__init__(self, maker, model, year, base_price, miles)
 
-    def sale_price(self):
-        return self.base_price * 1.6
 
-    def purchase_price(self):
-        return self.sale_price() - (0.004 * self.miles)
 
