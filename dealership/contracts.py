6bc5cccf0a526f5d3bb85f25fb12a09f1c1ee6f0
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-11-20 03:23:20 -0500

    total_value in buycontract class

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,52 @@
 class Contract(object):
-    pass
+
+    employee_discount = 0.1
 
 
 class BuyContract(Contract):
+
     def __init__(self, vehicle, customer, monthly_payments):
+
+        self.customer = customer
+        self.vehicle = vehicle
+        self.monthly_payments = monthly_payments
+
+    def total_value(self):
+
+        vehicle_sale_price = self.vehicle.sale_price()
+
+        # get interest rate depending on vehicly type
+        vehicle_type = type(self.vehicle).__name__
+        if vehicle_type == 'Car':
+            I_rate = 1.07
+
+        elif vehicle_type == 'Motorcycle':
+            I_rate = 1.03
+
+        elif vehicle_type == 'Truck':
+            I_rate = 1.11
+
+        # value before any discounts
+        value = vehicle_sale_price + (I_rate * self.monthly_payments * vehicle_sale_price) / 100
+
+        # apply discount if an employee
+        employee_status = self.customer.is_employee()
+        if employee_status:
+            return .90 * value
+
+        else:
+            return value
+
+    def monthly_value(self):
         pass
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
+        pass
+
+    def total_value(self):
+        pass
+
+    def monthly_value(self):
         pass
