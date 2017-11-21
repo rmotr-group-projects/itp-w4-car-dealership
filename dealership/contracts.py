9966bd920105c567a076221406bee9cd69ac0e94
J F <jordi.frank1994@gmail.com>
2017-11-21 03:39:23 +0000

    Final versions of each file which passes all tests.

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -4,9 +4,60 @@ class Contract(object):
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
-
-class LeaseContract(Contract):
+        self.vehicle = vehicle 
+        self.customer = customer 
+        self.monthly_payments = monthly_payments
+    
+    def discount_car(self, vehicle_price): 
+        return vehicle_price - vehicle_price * .1
+        
+    def total_value(self):
+        if self.vehicle.is_car() and self.customer.is_employee() == False:
+            return self.vehicle.sale_price() + (1.07 * self.monthly_payments * self.vehicle.sale_price() / 100)
+        elif self.vehicle.is_car() and self.customer.is_employee() == True:
+           car_price = self.vehicle.sale_price() + (1.07 * self.monthly_payments * self.vehicle.sale_price() / 100)
+           return self.discount_car(car_price)
+        elif self.vehicle.is_motorcycle() and self.customer.is_employee() == False:
+           return self.vehicle.sale_price() + (1.03 * self.monthly_payments * self.vehicle.sale_price() / 100)
+        elif self.vehicle.is_motorcycle() and self.customer.is_employee() == True:
+           bike_price = self.vehicle.sale_price() + (1.03 * self.monthly_payments * self.vehicle.sale_price() / 100)
+           return self.discount_car(bike_price)
+        elif self.vehicle.is_truck() and self.customer.is_employee() == False:
+           return self.vehicle.sale_price() + (1.11 * self.monthly_payments * self.vehicle.sale_price() / 100)
+        elif self.vehicle.is_truck() and self.customer.is_employee():
+           truck_price = self.vehicle.sale_price() + (1.11 * self.monthly_payments * self.vehicle.sale_price() / 100)
+           return self.discount_car(truck_price)
+    
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
+        
+    
+        
+class LeaseContract(BuyContract, Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+        if self.vehicle.is_car() and self.customer.is_employee() == False:
+            car_multiplier = self.vehicle.sale_price() * 1.2 / self.length_in_months
+            car_price = self.vehicle.sale_price() + car_multiplier
+            return car_price 
+        elif self.vehicle.is_car() and self.customer.is_employee() == True:
+            self.discount_car(car_price)
+        elif self.vehicle.is_motorcycle() and self.customer.is_employee() == False:
+           bike_multiplier = self.vehicle.sale_price() * 1 / self.length_in_months
+           bike_price = self.vehicle.sale_price() + bike_multiplier
+           return bike_price
+        elif self.vehicle.is_motorcycle() and self.customer.is_employee() == True:
+           self.discount_car(bike_price)
+        elif self.vehicle.is_truck() and self.customer.is_employee() == False:
+           truck_multiplier = self.vehicle.sale_price() * 1.7 / self.length_in_months
+           truck_price = self.vehicle.sale_price() + truck_multiplier
+           return truck_price
+        elif self.vehicle.is_truck() and self.customer.is_employee():
+           self.discount_car(truck_price)
+    
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,60 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker 
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
 
 class Car(Vehicle):
-    pass
-
+    def __init__(self, maker, model, year, base_price, miles):
+        super(Car, self).__init__(maker, model, year, base_price, miles)
+    
+    def sale_price(self):
+        return self.base_price * 1.2
+    
+    def purchase_price(self):
+        return self.sale_price() - (0.004 * self.miles)
+        
+    def is_car(self):
+        return True 
+    def is_motorcycle(self):
+        return False 
+    def is_truck(self):
+        return False 
 
 class Motorcycle(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        super(Motorcycle, self).__init__(maker, model, year, base_price, miles)
+    
+    def sale_price(self):
+        return self.base_price * 1.1
+    
+    def purchase_price(self):
+        return self.sale_price() - (0.009 * self.miles)
+    
+    def is_car(self):
+        return False 
+    def is_motorcycle(self):
+        return True
+    def is_truck(self):
+        return False 
 
 
 class Truck(Vehicle):
-    pass
+    def __init__(self, maker, model, year, base_price, miles):
+        super(Truck, self).__init__(maker, model, year, base_price, miles)
+    
+    def sale_price(self):
+        return self.base_price * 1.6
+    
+    def purchase_price(self):
+        return self.sale_price() - (0.02 * self.miles)
+    
+    def is_car(self):
+        return False 
+    def is_motorcycle(self):
+        return False 
+    def is_truck(self):
+        return True 
