904197cff7a716d0363f05c4ec576a260b26f7ab
Karey Shumansky <kareyshumansky@gmail.com>
2017-11-21 03:30:35 +0000

    committing changes to car dealship project

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,42 @@
 class Contract(object):
-    pass
 
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
+        
+    def total_value(self):
+        base_contract_value = self.vehicle.sale_price() + self.get_contract_modifier()
+        return base_contract_value - (0.1*base_contract_value*self.customer.is_employee())
+        
+    def monthly_value(self):
+        return self.total_value()/self.get_monthly_modifier()
+        
+    def get_contract_modifier(self):
+        raise NotImplementedError()
+        
+    def get_monthly_modifier(self):
+        raise NotImplementedError()
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.monthly_payments = monthly_payments
+        self.vehicle = vehicle
+        self.customer = customer
 
+    def get_contract_modifier(self):
+        return (self.vehicle.get_buy_multiplier() * self.monthly_payments * self.vehicle.sale_price() / 100)
+
+    def get_monthly_modifier(self):
+        return self.monthly_payments
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.length_in_months = length_in_months
+        self.vehicle = vehicle
+        self.customer = customer
+        
+    def get_contract_modifier(self):
+        return ((self.vehicle.get_lease_multiplier() * self.vehicle.sale_price())/self.length_in_months)
+        
+    def get_monthly_modifier(self):
+        return self.length_in_months
