958d42cfa2aff7ab52ae2948e1a33a08e8ed7f33
Kevin Geiszler <kevingeiszler90@gmail.com>
2017-11-22 05:35:13 +0000

    Submit project

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,75 @@
+from customers import Customer, Employee
+from vehicles import Car, Truck, Motorcycle
+
 class Contract(object):
-    pass
+    '''Represents a contract with the dealership. Must be implemented as a
+    BuyContract or a LeaseContract.'''
+    def __init__(self, vehicle, customer, num_payments):
+        self.vehicle = vehicle
+        self.customer = customer
+        self.discount_rate = 0.10
+        
+        if isinstance(self, BuyContract):
+            self.monthly_payments = num_payments
+        elif isinstance(self, LeaseContract):
+            self.length_in_months = num_payments
+        else:
+            raise NotImplementedError()
+    
+    def total_value(self):
+        raise NotImplementedError()
+    
+    def monthly_value(self):
+        raise NotImplementedError()
 
 
 class BuyContract(Contract):
+    '''Represents a contract for purchasing a vehicle.'''
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract, self).__init__(vehicle, customer, monthly_payments)
+        
+        if isinstance(self.vehicle, Car):
+            self.interest_rate = 0.07
+        elif isinstance(self.vehicle, Motorcycle):
+            self.interest_rate = 0.03
+        elif isinstance(self.vehicle, Truck):
+            self.interest_rate = 0.11
+    
+    def total_value(self):
+        '''Calculates the total value of the purchasing contact.'''
+        value = self.vehicle.sale_price() + ((1 + self.interest_rate) *
+                        self.monthly_payments * self.vehicle.sale_price() / 100)
+        if self.customer.is_employee():
+            return value * (1 - self.discount_rate)
+        return value
+        
+    def monthly_value(self):
+        '''Calculates the monthly payment for purchasing a vehicle.'''
+        return self.total_value() / self.monthly_payments
 
 
 class LeaseContract(Contract):
+    '''Represents a contract for leasing a vehicle.'''
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer, length_in_months)
+
+        if isinstance(self.vehicle, Car):
+            self.lease_rate = 0.20
+        elif isinstance(self.vehicle, Motorcycle):
+            self.lease_rate = 0
+        elif isinstance(self.vehicle, Truck):
+            self.lease_rate = 0.70
+            
+        self.lease_multiplier = (self.vehicle.sale_price() * 
+                                  (1 + self.lease_rate) / self.length_in_months)
+                                  
+    def total_value(self):
+        '''Calculates the total value of the leasing contact.'''
+        value = self.vehicle.sale_price() + self.lease_multiplier
+        if self.customer.is_employee():
+            return value * (1 - self.discount_rate)
+        return value
+        
+    def monthly_value(self):
+        '''Calculates the monthly payment for leasing a vehicle.'''
+        return self.total_value() / self.length_in_months
--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,21 @@
 class Person(object):
+    '''Represents a person making a purchase at the car dealership.'''
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
+    def is_employee(self):
+        raise NotImplementedError
 
 
 class Customer(Person):
-    pass
-
+    '''Represents a typical customer.'''
+    def is_employee(self):
+        return False
+        
 
 class Employee(Person):
-    pass
+    '''Represents a customer who is also an employee.'''
+    def is_employee(self):
+        return True
