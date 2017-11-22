ea4bb499611b1eb563e55a78f538c0fab1ae81d9
WilliamTheBeginner <william78smart@hotmail.com>
2017-11-22 08:37:39 -0500

    chagnes

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,4 +1,5 @@
 from vehicles import *
+from customers import *
 
 class Contract(object):
     pass
@@ -9,29 +10,31 @@ class BuyContract(Contract):
         self.vehicle = vehicle
         self.customer = customer
         self.payment = monthly_payments
-
+        print(self.vehicle)
     def total_value(self):
-        if self.vehicle == 'car':
-            i = 1.07
+    	if isinstance(self.vehicle, Car):
+    		i = 1.07
+    	if isinstance(self.vehicle, Motorcycle):
+    		i = 1.03
+    	if isinstance(self.vehicle, Truck):
+    		i = 1.11
+    	if isinstance(self.customer, Employee):
+    		d = 0.9
+    	if isinstance(self.customer, Customer):
+    		d = 1
 
-        if self.vehicle == 'motorcycle':
-            i = 1.03
-
-        if self.vehicle == 'truck':
-            i = 1.1
-
-        if self.customer == 'customer':
-            d = 1
-
-        if self.customer == 'employee':
-            d = 0.9
-
-        return self.sale_price() + (i * self.monthly_payments * sale_price() / 100) * d
-
-    def monthy_value():
-        return self.total_value() / self.monthly_payments
+    	return self.vehicle.sale_price() + (i * self.payment * self.vehicle.sale_price() / 100) * d
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
         pass
 
+customer = Customer('John', 'Doe', 'john@example.com')
+
+
+car = Car(
+    maker='Ford', model='Mustang', year=2005, base_price=18000, miles=31000)
+car_contract = BuyContract(
+        vehicle=car, customer=customer, monthly_payments=6)
+
+print(car_contract.total_value())
