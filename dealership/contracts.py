486f2ec63e7791d3619bb77c9335e66dc1c40dac
William <william78smart@hotmail.com>
2017-11-22 14:08:55 +0000

    thing

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,5 +1,5 @@
 from vehicles import *
-
+from customers import *
 class Contract(object):
     pass
 
@@ -11,19 +11,22 @@ class BuyContract(Contract):
         self.payment = monthly_payments
         print(self.vehicle)
     def total_value(self):
-    	if isinstance(self.vehicle, car):
+    	if isinstance(self.vehicle, Car):
     		i = 1.07
-    	if isinstance(self.vehicle, motorcycle):
+    	if isinstance(self.vehicle, Motorcycle):
     		i = 1.03
-    	if isinstance(self.vehicle, truck):
+    	if isinstance(self.vehicle, Truck):
     		i = 1.11
-    	if isinstance(self.customer, employee):
+    	if isinstance(self.customer, Employee):
     		d = 0.9
-    	if isinstance(self.customer, customer):
+    	if isinstance(self.customer, Customer):
     		d = 1
 
     	return self.vehicle.sale_price() + (i * self.payment * self.vehicle.sale_price() / 100) * d
-
+    	
+    def monthly_value(self):
+        return self.total_value() / self.payment
+        
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
         pass
